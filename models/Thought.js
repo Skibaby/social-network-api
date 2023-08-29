31f2b7eff9fe93fcd064e96324c050dfbc096098
Yuski <yuskijoness@gmail.com>
2023-08-29 11:36:03 -0400

    thought.js file complete

---
+++ models/Thought.js
@@ -0,0 +1,52 @@
+const { Schema, models, Types} = require('mongoose');
+const reactionSchema = required('./reaction');
+
+const formatDate = () = {
+  return `${new Date().getUTCMonth()+ 1}/${new Date().getUTCMonth()}/${new Date().getUTCFullYear()}`;
+}
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: string,
+      required: true,
+      minLength: 1,
+      maxLength: 128
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+
+    },
+
+    username: {
+      type: string, 
+      required: true,
+    },
+    reactions: [reactionSchema] , 
+
+
+  },
+  {
+    toJSON: {
+
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+
+  }
+
+);
+
+thoughtSchema.virtual('reactionCount')
+.get(function () {
+    return this.reactions.length
+});
+
+const thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
